d00fff8d3b874cda04d8799ccc1f81056f8df9a4
jhlee9462 <jhlee9462@gmail.com>
2023-04-13 15:38:16 +0900

    75일차 끝

--- src/day13/practice/Main.java
+++ src/day13/practice/Main.java
@@ -3,6 +3,7 @@ package day13.practice;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 
 import static java.lang.System.out;
 import static java.util.Comparator.comparing;
@@ -32,7 +33,7 @@ public class Main {
                 .filter(transaction -> transaction.getYear() == 2021)
                 .sorted(comparing(Transaction::getValue))
                 .collect(toList());
-
+        out.println("collect1 = " + collect1);
 
         // 연습 2: 거래자가 근무하는 모든 도시이름을 중복 없이 나열하시오.
         // List<String>
@@ -43,34 +44,59 @@ public class Main {
                 .map(t -> t.getTrader().getCity())
                 .distinct()
                 .collect(toList());
-
+        out.println("collect2 = " + collect2);
 
         // 연습 3: Cambridge에 근무하는 모든 거래자를 찾아 거래자리스트로 이름순으로 오름차정렬.
         // List<Trader>
         List<Trader> collect3 = transactions.stream()
                 .map(Transaction::getTrader)
+                .distinct()
                 .filter(t -> t.getCity().equals("Cambridge"))
                 .sorted(comparing(Trader::getName))
                 .collect(toList());
+        out.println("collect3 = " + collect3);
 
         // 연습 4: 모든 거래자의 이름을 리스트에 모아서 알파벳순으로  오름차정렬하여 반환
         // List<String>
         List<String> collect4 = transactions.stream()
                 .map(t -> t.getTrader().getName())
+                .distinct()
                 .sorted(Comparator.naturalOrder())
                 .collect(toList());
+        out.println("collect4 = " + collect4);
 
         // 연습 5: Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?
-
-
+        boolean flag1 = transactions.stream()
+                .anyMatch(t -> t.getTrader().getCity().equals("Milan"));
+        out.println("flag1 = " + flag1);
 
         // 연습 6: Cambridge에 사는 거래자의 모든 거래액의 총합 출력.
-
+        int sum1 = transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .mapToInt(Transaction::getValue)
+                .sum();
+        out.println("sum1 = " + sum1);
 
         // 연습 7: 모든 거래에서 최고거래액은 얼마인가?
-
+        int maxValue = transactions.stream()
+                .mapToInt(Transaction::getValue)
+                .max()
+                .getAsInt();
+        out.println("maxValue = " + maxValue);
 
         // 연습 8. 가장 작은 거래액을 가진 거래정보 탐색
+        List<Transaction> minValueTransaction = transactions.stream()
+                .filter(t -> t.getValue() == transactions.stream()
+                        .mapToInt(Transaction::getValue)
+                        .min()
+                        .getAsInt())
+                .collect(toList());
 
+        Transaction minTransaction = transactions.stream()
+                .min(comparing(Transaction::getValue))
+                .get();
+
+        out.println("minValueTransaction = " + minValueTransaction);
+        out.println("minTransaction = " + minTransaction);
     }
 }
